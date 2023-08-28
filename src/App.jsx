fb608ad5e4ed99c003aa0b9bc3dafcbb94b09980
TCAA-Web <kfry@techcollege.dk>
2023-08-28 09:01:32 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { MainLayout } from "./layout/MainLayout";
+import { HomePage } from "./pages/HomePage";
+import { AboutPage } from "./pages/AboutPage";
+import { BlogPage } from "./pages/BlogPage";
+import { NotFoundPage } from "./pages/NotFoundPage";
+
+function App() {
+  return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout/>}>
+            <Route index element={<HomePage/>}/>
+            <Route path="/about" element={<AboutPage/>}/>
+            <Route path="/blog" element={<BlogPage/>}/>
+            <Route path="*" element={<NotFoundPage/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+  );
+}
+
+export default App;

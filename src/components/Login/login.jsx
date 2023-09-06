18d76f9d9a2c1d91a3cfd49f8cfcbb1f2d068b84
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-09-06 10:18:42 +0200

    new update alert

--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -2,6 +2,34 @@ export const Login = () => {
 
     const submitHandler = (event) => {
         event.preventDefault()
+
+        const username = event.target.username.value
+        const password = event.target.password.value
+
+
+if (username !== "" &&  password !== ""  ) {
+
+    const url = "'https://dummyjson.com/auth/login'";
+    const options = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      
+      username: username,
+      password:  password,
+    })}
+
+    fetch (url, options)
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .then((err) => console.error(err));
+
+
+}
+else {
+    alert('du skal udfylde felterne!')
+}
+
         console.log("username:", event.target.username.value)
         console.log("password:", event.target.password.value)
 
